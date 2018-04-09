ed765c52a4db31bfbe5599449b4f2dd0c439ae31
jmorgan20 <37450743+jmorgan20@users.noreply.github.com>
2018-04-09 17:35:15 -0500

    Completed

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -10,7 +10,44 @@
 public class StarFigures {
 
 	public static void main(String[] args) {
+		drawImageOne();
+		drawBlankLines();
+		drawImageTwo();
+		drawBlankLines();
+		drawImageThree();		
 
+	}
+	private static void drawImageThree() {
+		drawbar();
+		drawline();
+		drawstar();
+	}
+	private static void drawImageTwo() {
+		drawline();
+		drawstar();
+		drawline();
+	}
+	private static void drawImageOne() {
+		drawline();
+		drawstar();
+	}
+	private static void drawBlankLines() {
+		System.out.println();
+		System.out.println();
+	}
+	public static void drawline() {
+		System.out.println("*****");
+		System.out.println("*****");
+	}
+	public static void drawstar() {
+		System.out.println(" * *");
+		System.out.println("  *");
+		System.out.println(" * *");
+	}
+	public static void drawbar() {
+		System.out.println("  *");
+		System.out.println("  *");
+		System.out.println("  *");
 	}
 
 }
--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,29 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):jason morgan
+  	  	Date: 
 */
 
-// 1. Your predicted output 
- 
+// 1. Your predicted output done with main this is message one this is message two  done with message two
 
 public class Tricky {
+	public static void main(String[] args) {
+		message1();
+		message2();
+		System.out.println("Done with main.");
+	}
 
+	public static void message1() {
+		System.out.println("This is message1.");
+	}
+
+	public static void message2() {
+		System.out.println("This is message2.");
+		message1();
+		System.out.println("Done with message 2.");
+	}
 }
 
 // 3. Were you correct? Explain any differences
- 
- 
-
+// no, I did not notice that the message1 and2 were also before the done with
+// main.
